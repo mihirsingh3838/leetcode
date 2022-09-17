1b88c1bfe64b05917e3b850a30da5197b9dd7228
mihirsingh3838 <mihirsingh252001@gmail.com>
2022-09-17 07:59:29 +0530

    Compliment of base 10 integer

---
+++ 1009.complement-of-base-10-integer.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=1009 lang=cpp
+ *
+ * [1009] Complement of Base 10 Integer
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        int m=n;
+        int mask=0;
+
+        if(n==0)
+        {
+            return 1;
+        }
+
+        while(m!=0)
+        {
+            mask= (mask<<1)|1;
+            m=m>>1;
+        }
+        int ans= (~n)&mask;
+        return ans;
+    }
+};
+// @lc code=end
+
